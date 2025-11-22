e22baa16fb3929cde281ac532c9fa130fdf14c84
Mohamad Naim <202210865@ua.edu.lb>
2025-11-22 16:41:36 +0200

    Use Azure Key Vault + SQL Server connection

--- config/database.php
+++ config/database.php
@@ -42,6 +42,13 @@ return [
             'synchronous' => null,
             'transaction_mode' => 'DEFERRED',
         ],
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'url' => env('AZURE_DB_CONNECTION'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            ],
+
 
         'mysql' => [
             'driver' => 'mysql',
@@ -98,25 +105,9 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
-
     ],
 
     /*
-    |--------------------------------------------------------------------------
     | Migration Repository Table
     |--------------------------------------------------------------------------
     |
